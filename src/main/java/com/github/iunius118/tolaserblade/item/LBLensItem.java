b0d9b01ff6ed42a716ec01b77768de67582cb338
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2020-02-23 08:34:44 +0900

    Add item classes of Laser Blade parts

---
+++ src/main/java/com/github/iunius118/tolaserblade/item/LBLensItem.java
@@ -0,0 +1,12 @@
+package com.github.iunius118.tolaserblade.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class LBLensItem extends Item implements ToLaserBladeItemGroup {
+    public static Item.Properties properties = (new Item.Properties()).setNoRepair().group(ItemGroup.TOOLS);
+
+    public LBLensItem() {
+        super(properties);
+    }
+}
---
+++ src/main/java/com/github/iunius118/tolaserblade/item/LBMediumItem.java
@@ -0,0 +1,12 @@
+package com.github.iunius118.tolaserblade.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class LBMediumItem extends Item implements ToLaserBladeItemGroup {
+    public static Item.Properties properties = (new Item.Properties()).setNoRepair().group(ItemGroup.TOOLS);
+
+    public LBMediumItem() {
+        super(properties);
+    }
+}
