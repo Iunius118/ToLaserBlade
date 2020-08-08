2050bfcbe1fc7857bd893e0c5ecb53cbd1be586a
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2020-08-08 15:15:43 +0900

    Fix access modifier of item group class

--- src/main/java/com/github/iunius118/tolaserblade/item/ModMainItemGroup.java
+++ src/main/java/com/github/iunius118/tolaserblade/item/ModMainItemGroup.java
@@ -7,7 +7,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 import static com.github.iunius118.tolaserblade.ToLaserBlade.MOD_ID;
 
-interface ModMainItemGroup {
+public interface ModMainItemGroup {
     ItemGroup ITEM_GROUP = new ItemGroup(MOD_ID) {
         @Override
         @OnlyIn(Dist.CLIENT)
