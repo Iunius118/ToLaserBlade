6391f490f4156527b3e7fede269658c947961bf1
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2020-09-14 22:16:21 +0900

    Add upgrade recipes to recipe provider

--- src/main/java/com/github/iunius118/tolaserblade/data/TLBRecipeProvider.java
+++ src/main/java/com/github/iunius118/tolaserblade/data/TLBRecipeProvider.java
@@ -1,13 +1,17 @@
 package com.github.iunius118.tolaserblade.data;
 
 import com.github.iunius118.tolaserblade.item.ModItems;
+import com.github.iunius118.tolaserblade.laserblade.upgrade.Upgrade;
+import com.github.iunius118.tolaserblade.laserblade.upgrade.UpgradeManager;
 import net.minecraft.data.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class TLBRecipeProvider extends RecipeProvider implements IConditionBuilder {
@@ -54,8 +58,8 @@ public class TLBRecipeProvider extends RecipeProvider implements IConditionBuild
         // Netherite Laser Blade by using Smithing Table
         addSmithingRecipe(Ingredient.fromItems(ModItems.LASER_BLADE), Ingredient.fromItems(Items.NETHERITE_INGOT), ModItems.LASER_BLADE_FP, Items.NETHERITE_INGOT, consumer);
 
-        // Netherite Laser Blade Casing by using Smithing Table
-        addSmithingRecipe(Ingredient.fromItems(ModItems.LB_CASING), Ingredient.fromItems(Items.NETHERITE_INGOT), ModItems.LB_CASING_FP, Items.NETHERITE_INGOT, consumer);
+        // Upgrade Recipes
+        addUpgradeRecipes(consumer);
     }
 
     @Override
@@ -68,5 +72,17 @@ public class TLBRecipeProvider extends RecipeProvider implements IConditionBuild
         SmithingRecipeBuilder.func_240502_a_(base, addition, result)
                 .func_240503_a_("has_" + criterionItem.getRegistryName().getPath(), hasItem(criterionItem))
                 .func_240504_a_(consumer, result.getRegistryName().toString() + "_smithing");
+    }
+
+    private void addUpgradeRecipes(Consumer<IFinishedRecipe> consumer) {
+        Map<ResourceLocation, Upgrade> upgrades = UpgradeManager.getUpgrades();
+        upgrades.forEach((id, upgrade) -> {
+            UpgradeRecipeBuilder.upgradeRecipe(Ingredient.fromItems(ModItems.LASER_BLADE), upgrade.getIngredient(), id)
+                    .addCriterion("has_laser_blade", hasItem(ModItems.LASER_BLADE))
+                    .build(consumer, id.toString() + "_upgrade");
+            UpgradeRecipeBuilder.upgradeRecipe(Ingredient.fromItems(ModItems.LASER_BLADE_FP), upgrade.getIngredient(), id)
+                    .addCriterion("has_laser_blade_fp", hasItem(ModItems.LASER_BLADE_FP))
+                    .build(consumer, id.toString() +  "_fp_upgrade");
+        });
     }
 }
