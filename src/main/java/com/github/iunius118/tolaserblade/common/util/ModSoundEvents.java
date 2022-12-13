54a5a4b20647997fdd516c80fd9338537fed8af5
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2022-12-14 01:54:22 +0900

    Update sound events to 1.19.3

--- src/main/java/com/github/iunius118/tolaserblade/common/util/ModSoundEvents.java
+++ src/main/java/com/github/iunius118/tolaserblade/common/util/ModSoundEvents.java
@@ -5,7 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
 public class ModSoundEvents {
-    public static final SoundEvent ITEM_DX_LASER_BLADE_SWING = new SoundEvent(new ResourceLocation(ToLaserBlade.MOD_ID, "item.dx_laser_blade.swing"));
-    public static final SoundEvent ITEM_LASER_BLADE_SWING = new SoundEvent(new ResourceLocation(ToLaserBlade.MOD_ID, "item.laser_blade.swing"));
-    public static final SoundEvent ITEM_LASER_BLADE_FP_SWING = new SoundEvent(new ResourceLocation(ToLaserBlade.MOD_ID, "item.laser_blade_fp.swing"));
+    public static final SoundEvent ITEM_DX_LASER_BLADE_SWING = SoundEvent.createVariableRangeEvent(new ResourceLocation(ToLaserBlade.MOD_ID, "item.dx_laser_blade.swing"));
+    public static final SoundEvent ITEM_LASER_BLADE_SWING = SoundEvent.createVariableRangeEvent(new ResourceLocation(ToLaserBlade.MOD_ID, "item.laser_blade.swing"));
+    public static final SoundEvent ITEM_LASER_BLADE_FP_SWING = SoundEvent.createVariableRangeEvent(new ResourceLocation(ToLaserBlade.MOD_ID, "item.laser_blade_fp.swing"));
 }
