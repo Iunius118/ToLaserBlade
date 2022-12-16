05d5e49bd6467bead897214cef917e22e0294199
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2022-12-16 21:13:05 +0900

    Change ToLaserBladeAPI

--- src/main/java/com/github/iunius118/tolaserblade/api/ToLaserBladeAPI.java
+++ src/main/java/com/github/iunius118/tolaserblade/api/ToLaserBladeAPI.java
@@ -3,30 +3,20 @@ package com.github.iunius118.tolaserblade.api;
 import com.github.iunius118.tolaserblade.api.client.model.LaserBladeModel;
 
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ToLaserBladeAPI {
-    private static Object modelManagerInstance;
-    private static Method addInternalModel;
-
-    public static void addInternalModel(int index, Supplier<? extends LaserBladeModel> model) {
-        if (modelManagerInstance == null || addInternalModel == null) {
-            init();
-        }
-
+    public static void addModels(List<LaserBladeModel> models) {
         try {
-            addInternalModel.invoke(modelManagerInstance, index, model);
-        } catch (ReflectiveOperationException e) {
-            e.printStackTrace();
-        }
-    }
+            Class<?> laserBladeModelManager = Class.forName("com.github.iunius118.tolaserblade.client.model.LaserBladeModelManager");
+            Method getInstance = laserBladeModelManager.getMethod("getInstance");
+            Object modelManagerInstance = getInstance.invoke(null);
+            Method addModel = laserBladeModelManager.getMethod("addModel", Supplier.class);
 
-    private static void init() {
-        try {
-            Class<? extends Object> laserBladeInternalModelManager = Class.forName("com.github.iunius118.tolaserblade.client.model.LaserBladeInternalModelManager");
-            Method getInstance = laserBladeInternalModelManager.getMethod("getInstance");
-            modelManagerInstance = getInstance.invoke(null);
-            addInternalModel = laserBladeInternalModelManager.getMethod("addInternalModel", int.class, Supplier.class);
+            for(LaserBladeModel model : models) {
+                addModel.invoke(modelManagerInstance, model);
+            }
         } catch (ReflectiveOperationException e) {
             e.printStackTrace();
         }
