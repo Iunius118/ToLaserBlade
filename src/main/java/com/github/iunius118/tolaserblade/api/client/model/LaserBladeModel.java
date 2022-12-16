284d438e69503691099265c6d90f17e93818a167
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2022-12-16 21:08:29 +0900

    Change laser blade model interface

--- src/main/java/com/github/iunius118/tolaserblade/api/client/model/LaserBladeModel.java
+++ src/main/java/com/github/iunius118/tolaserblade/api/client/model/LaserBladeModel.java
@@ -3,11 +3,12 @@ package com.github.iunius118.tolaserblade.api.client.model;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
 public interface LaserBladeModel {
-    void render(ItemStack itemStack, ItemTransforms.TransformType mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay);
+    default int getID() {
+        return -1;
+    }
 
-    ResourceLocation getTexture();
+    void render(ItemStack itemStack, ItemTransforms.TransformType mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay);
 }
