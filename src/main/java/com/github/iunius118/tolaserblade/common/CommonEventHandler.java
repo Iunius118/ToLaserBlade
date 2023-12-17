c110502bf109a0b012e3efc3528069df0cd7265e
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-12-17 23:25:19 +0900

    Remove remapping of item IDs for backward compatibility

--- src/main/java/com/github/iunius118/tolaserblade/common/CommonEventHandler.java
+++ src/main/java/com/github/iunius118/tolaserblade/common/CommonEventHandler.java
@@ -1,41 +1,13 @@
 package com.github.iunius118.tolaserblade.common;
 
-import com.github.iunius118.tolaserblade.ToLaserBlade;
 import com.github.iunius118.tolaserblade.client.ClientModEventHandler;
 import com.github.iunius118.tolaserblade.config.ToLaserBladeConfig;
-import com.github.iunius118.tolaserblade.world.item.ModItems;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 
 public class CommonEventHandler {
-    /*
-     * Remapping Items
-     */
-
-    @SubscribeEvent
-    public static void remapItems(MissingMappingsEvent event) {
-        if (event.getKey() != ForgeRegistries.Keys.ITEMS)
-            return;
-
-        final Map<ResourceLocation, Item> remappingItemMap = new HashMap<>();
-        // Replace item ID "tolaserblade:tolaserblade.laser_blade" (-1.11.2) with "tolaserblade:laser_blade" (1.12-)
-        remappingItemMap.put(new ResourceLocation(ToLaserBlade.MOD_ID, "tolaserblade.laser_blade"), ModItems.LASER_BLADE);
-
-        // Replace item ID "tolaserblade:lasar_blade" (-1.14.4) with "tolaserblade:dx_laser_blade" (1.15-)
-        remappingItemMap.put(new ResourceLocation(ToLaserBlade.MOD_ID, "lasar_blade"), ModItems.DX_LASER_BLADE);
-
-        // Replace item ID "tolaserblade:laser_blade_core" (-1.14.4) with "tolaserblade:lb_broken" (1.15-)
-        remappingItemMap.put(new ResourceLocation(ToLaserBlade.MOD_ID, "laser_blade_core"), ModItems.LB_BROKEN);
-
-        // Replace item IDs
-        event.getAllMappings(ForgeRegistries.Keys.ITEMS).stream()
-                .filter(mapping -> mapping.getKey().getNamespace().equals(ToLaserBlade.MOD_ID) && remappingItemMap.containsKey(mapping.getKey()))
-                .forEach(mapping -> mapping.remap(remappingItemMap.get(mapping.getKey())));
-    }
-
     /*
      * World Events
      */
