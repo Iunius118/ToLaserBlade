17ca98ef0dd9bb382ba5f9a66555e85dc73e2ad8
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-06-23 01:41:38 +0900

    Add repair upgrade and fireproof upgrade

---
+++ src/main/java/com/github/iunius118/tolaserblade/core/laserblade/upgrade/RepairUpgrader.java
@@ -0,0 +1,23 @@
+package com.github.iunius118.tolaserblade.core.laserblade.upgrade;
+
+import com.github.iunius118.tolaserblade.world.item.ModItems;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class RepairUpgrader implements Upgrader {
+    @Override
+    public boolean canApply(ItemStack base, ItemStack addition) {
+        return true;
+    }
+
+    @Override
+    public UpgradeResult apply(ItemStack base, int baseCost) {
+        Item brandNew = base.has(DataComponents.FIRE_RESISTANT) ? ModItems.LB_BRAND_NEW_FP : ModItems.LB_BRAND_NEW;
+        // Copy item data to (fireproof) brand-new laser blade
+        ItemStack newStack = base.transmuteCopy(brandNew, base.getCount());
+        // Remove attribute modifiers from old stack
+        newStack.remove(DataComponents.ATTRIBUTE_MODIFIERS);
+        return UpgradeResult.of(newStack, 1);
+    }
+}
