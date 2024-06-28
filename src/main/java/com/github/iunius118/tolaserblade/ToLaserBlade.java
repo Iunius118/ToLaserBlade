4022aeaffd06eafdfd30df7f22608b8cfa695a48
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-06-28 20:54:33 +0900

    [1.21] Add DatapackBuiltinEntriesProvider to provide mod enchantment

--- src/main/java/com/github/iunius118/tolaserblade/ToLaserBlade.java
+++ src/main/java/com/github/iunius118/tolaserblade/ToLaserBlade.java
@@ -6,7 +6,8 @@ import com.github.iunius118.tolaserblade.client.model.LaserBladeModelManager;
 import com.github.iunius118.tolaserblade.common.CommonEventHandler;
 import com.github.iunius118.tolaserblade.common.RegistryEventHandler;
 import com.github.iunius118.tolaserblade.config.ToLaserBladeConfig;
-import com.github.iunius118.tolaserblade.data.*;
+import com.github.iunius118.tolaserblade.data.TLBDataGenerator;
+import com.github.iunius118.tolaserblade.data.TLBOldRecipeProvider6;
 import com.github.iunius118.tolaserblade.world.item.ItemEventHandler;
 import com.github.iunius118.tolaserblade.world.item.ModCreativeModeTabs;
 import com.mojang.logging.LogUtils;
@@ -17,7 +18,6 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.slf4j.Logger;
 
 @Mod(ToLaserBlade.MOD_ID)
@@ -36,7 +36,7 @@ public class ToLaserBlade {
 
         // Register event handlers
         RegistryEventHandler.registerGameObjects(modEventBus);
-        modEventBus.addListener(this::gatherData);
+        modEventBus.addListener(TLBDataGenerator::gatherData);
         modEventBus.addListener(ModCreativeModeTabs::onCreativeModeTabBuildContents);
         modEventBus.addListener(TLBOldRecipeProvider6::addPackFinders);
         NeoForge.EVENT_BUS.register(CommonEventHandler.class);
@@ -54,26 +54,5 @@ public class ToLaserBlade {
     }
 
     // Generate Data
-    private void gatherData(final GatherDataEvent event) {
-        var dataGenerator = event.getGenerator();
-        var packOutput = dataGenerator.getPackOutput();
-        var lookupProvider = event.getLookupProvider();
-        var existingFileHelper = event.getExistingFileHelper();
-        var blockTagsProvider = new TLBBlockTagsProvider(packOutput, lookupProvider, existingFileHelper);
 
-        // Server
-        final boolean includesServer = event.includeServer();
-        dataGenerator.addProvider(includesServer, new TLBRecipeProvider(packOutput, lookupProvider));
-        dataGenerator.addProvider(includesServer, blockTagsProvider);
-        dataGenerator.addProvider(includesServer, new TLBItemTagsProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
-        dataGenerator.addProvider(includesServer, new TLBEntityTypeTagsProvider(packOutput, lookupProvider, existingFileHelper));
-        dataGenerator.addProvider(includesServer, new TLBAdvancementProvider(packOutput, lookupProvider, existingFileHelper));
-        TLBOldRecipeProvider6.addProviders(event);
-
-        // Client
-        final boolean includesClient = event.includeClient();
-        dataGenerator.addProvider(includesClient, new TLBItemModelProvider(packOutput, existingFileHelper));
-        TLBLanguageProvider.addProviders(includesClient, dataGenerator, packOutput);
-        dataGenerator.addProvider(includesClient, new TLBSoundDefinitionsProvider(packOutput, existingFileHelper));
-    }
 }
---
+++ src/main/java/com/github/iunius118/tolaserblade/data/TLBDataGenerator.java
@@ -0,0 +1,52 @@
+package com.github.iunius118.tolaserblade.data;
+
+import com.github.iunius118.tolaserblade.ToLaserBlade;
+import com.github.iunius118.tolaserblade.world.item.enchantment.LightElementEnchantment;
+import com.github.iunius118.tolaserblade.world.item.enchantment.ModEnchantments;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.Set;
+
+public class TLBDataGenerator {
+    public static void gatherData(final GatherDataEvent event) {
+        var dataGenerator = event.getGenerator();
+        var packOutput = dataGenerator.getPackOutput();
+        var builtinEntriesProvider = new DatapackBuiltinEntriesProvider(packOutput, event.getLookupProvider(), getEntriesBuilder(), Set.of(ToLaserBlade.MOD_ID));
+        var lookupProvider = builtinEntriesProvider.getRegistryProvider();
+        var existingFileHelper = event.getExistingFileHelper();
+        var blockTagsProvider = new TLBBlockTagsProvider(packOutput, lookupProvider, existingFileHelper);
+
+        // Server
+        final boolean includesServer = event.includeServer();
+        dataGenerator.addProvider(includesServer, builtinEntriesProvider);
+        dataGenerator.addProvider(includesServer, new TLBRecipeProvider(packOutput, lookupProvider));
+        dataGenerator.addProvider(includesServer, blockTagsProvider);
+        dataGenerator.addProvider(includesServer, new TLBItemTagsProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
+        dataGenerator.addProvider(includesServer, new TLBEntityTypeTagsProvider(packOutput, lookupProvider, existingFileHelper));
+        dataGenerator.addProvider(includesServer, new TLBEnchantmentTagsProvider(packOutput, lookupProvider, existingFileHelper));
+        dataGenerator.addProvider(includesServer, new TLBAdvancementProvider(packOutput, lookupProvider, existingFileHelper));
+        TLBOldRecipeProvider6.addProviders(event);
+
+        // Client
+        final boolean includesClient = event.includeClient();
+        dataGenerator.addProvider(includesClient, new TLBItemModelProvider(packOutput, existingFileHelper));
+        TLBLanguageProvider.addProviders(includesClient, dataGenerator, packOutput);
+        dataGenerator.addProvider(includesClient, new TLBSoundDefinitionsProvider(packOutput, existingFileHelper));
+    }
+
+    private static RegistrySetBuilder getEntriesBuilder() {
+        RegistrySetBuilder.RegistryBootstrap<Enchantment> enchantmentRegistry = bootstrapContext -> {
+            HolderGetter<Item> itemGetter = bootstrapContext.lookup(Registries.ITEM);
+            HolderGetter<Enchantment> enchantmentGetter = bootstrapContext.lookup(Registries.ENCHANTMENT);
+            // Register Light Element to bootstrap context
+            bootstrapContext.register(ModEnchantments.LIGHT_ELEMENT, LightElementEnchantment.get(itemGetter, enchantmentGetter));
+        };
+        return new RegistrySetBuilder().add(Registries.ENCHANTMENT, enchantmentRegistry);
+    }
+}
