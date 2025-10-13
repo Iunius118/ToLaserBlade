e9c0735246cf78fa2dfd1ece9305813791d30a1e
iunius118 <18502177+Iunius118@users.noreply.github.com>
2025-10-13 16:24:08 +0900

    Fix update of laser blade's attribute modifiers - Add onInit to ItemStackMixin

--- src/main/java/com/github/iunius118/tolaserblade/mixin/ItemStackMixin.java
+++ src/main/java/com/github/iunius118/tolaserblade/mixin/ItemStackMixin.java
@@ -3,7 +3,9 @@ package com.github.iunius118.tolaserblade.mixin;
 import com.github.iunius118.tolaserblade.world.item.LBSwordItem;
 import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
+    @Inject(method = "<init>(Lnet/minecraft/world/level/ItemLike;ILnet/minecraft/core/component/PatchedDataComponentMap;)V", at = @At("TAIL"))
+    private void onInit(ItemLike item, int count, PatchedDataComponentMap components, CallbackInfo ci) {
+        LBSwordItem.updateItemAttributeModifiers(ItemStack.class.cast(this));
+    }
+
     @Inject(method = "applyComponentsAndValidate(Lnet/minecraft/core/component/DataComponentPatch;)V", at = @At("TAIL"))
     private void onApplyComponentsAndValidate(DataComponentPatch dataComponentPatch, CallbackInfo ci) {
         LBSwordItem.updateItemAttributeModifiers(ItemStack.class.cast(this));
