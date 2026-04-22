823803fcdf655144fb737c6d4609e7974293a003
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-04-22 23:17:46 +0900

    Add SpecialModelWrapperMixin to get item display context for laser blade rendering

---
+++ src/main/java/com/github/iunius118/tolaserblade/mixin/client/SpecialModelWrapperMixin.java
@@ -0,0 +1,36 @@
+package com.github.iunius118.tolaserblade.mixin.client;
+
+import com.github.iunius118.tolaserblade.client.renderer.LBSwordRenderContext;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.renderer.item.ItemModelResolver;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+import net.minecraft.client.renderer.item.SpecialModelWrapper;
+import net.minecraft.world.entity.ItemOwner;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(SpecialModelWrapper.class)
+public abstract class SpecialModelWrapperMixin<T> implements ItemModel {
+	@Inject(method = "update(Lnet/minecraft/client/renderer/item/ItemStackRenderState;" +
+			"Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/client/renderer/item/ItemModelResolver;" +
+			"Lnet/minecraft/world/item/ItemDisplayContext;Lnet/minecraft/client/multiplayer/ClientLevel;" +
+			"Lnet/minecraft/world/entity/ItemOwner;I)V",
+			at = @At(value = "INVOKE",
+					target = "Lnet/minecraft/client/renderer/item/ItemStackRenderState$LayerRenderState;" +
+							"setExtents(Ljava/util/function/Supplier;)V"),
+			locals = LocalCapture.CAPTURE_FAILSOFT)
+	private void onUpdate(ItemStackRenderState output, ItemStack item, ItemModelResolver resolver,
+						  ItemDisplayContext displayContext, ClientLevel level, ItemOwner owner, int seed,
+						  CallbackInfo ci, ItemStackRenderState.LayerRenderState layer, T argument) {
+		// Set the ItemDisplayContext in the RenderContext for use in laser blade rendering.
+		if (argument instanceof LBSwordRenderContext context) {
+			context.displayContext = displayContext;
+		}
+	}
+}
