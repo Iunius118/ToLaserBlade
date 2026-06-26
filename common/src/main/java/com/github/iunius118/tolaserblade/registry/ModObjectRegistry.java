706ec960a03b746ebba0e5874bc8c1cee8450650
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-06-26 20:50:57 +0900

    Improve mod object registration

--- common/src/main/java/com/github/iunius118/tolaserblade/registry/ModObjectRegistry.java
+++ common/src/main/java/com/github/iunius118/tolaserblade/registry/ModObjectRegistry.java
@@ -2,7 +2,6 @@ package com.github.iunius118.tolaserblade.registry;
 
 import net.minecraft.core.Holder;
 
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -21,16 +20,41 @@ public interface ModObjectRegistry<V, T extends V> {
     Holder<V> register(String name, Supplier<T> object);
 
     /**
-     * Registers entries using the given method for bulk registration.
-     *
-     * @param registry Consumer for invoking the entry registration method.
-     * @return This instance.
-     */
-    ModObjectRegistry<V, T> registerObjects(Consumer<BiFunction<String, Supplier<T>, Holder<V>>> registry);
-
-    /**
      * Registers the entries registered on Forge-based platforms to the event.
      * This method MUST be called after entry registration has completed.
      */
     void register();
+
+    /**
+     * Registers entries using the given registry for bulk registration.
+     * After the registration, {@link ModObjectRegistry#register()} is called automatically.
+     *
+     * @param registry Consumer for invoking the entry registration method.
+     */
+    default void registerObjects(Consumer<ObjectRegistry<V, T>> registry){
+        registry.accept(new ObjectRegistry<>(this));
+        register();
+    }
+
+    /**
+     * Class that provides a method for registering modded object.
+     */
+    class ObjectRegistry<V, T extends V> {
+        private final ModObjectRegistry<V, T> registry;
+
+        ObjectRegistry(ModObjectRegistry<V, T> registry) {
+            this.registry = registry;
+        }
+
+        /**
+         * Adds a new entry to register.
+         *
+         * @param name The new entry's name.
+         * @param object The new entry to register.
+         * @return A holder for this entry.
+         */
+        public Holder<V> register(String name, Supplier<T> object) {
+            return registry.register(name, object);
+        }
+    }
 }
--- common/src/main/java/com/github/iunius118/tolaserblade/registry/ModRegistries.java
+++ common/src/main/java/com/github/iunius118/tolaserblade/registry/ModRegistries.java
@@ -20,48 +20,42 @@ public class ModRegistries {
         // Blocks
         Services.PLATFORM.createModObjectRegistry(BuiltInRegistries.BLOCK, Constants.MOD_ID)
                 .registerObjects(r -> {
-                    r.apply(Constants.Blocks.BL_BLUEPRINT.getPath(), () -> ModBlocks.BL_BLUEPRINT);
-                })
-                .register();
+                    r.register(Constants.Blocks.BL_BLUEPRINT.getPath(), () -> ModBlocks.BL_BLUEPRINT);
+                });
         // Block types
         Services.PLATFORM.createModObjectRegistry(BuiltInRegistries.BLOCK_TYPE, Constants.MOD_ID)
                 .registerObjects(r -> {
-                    r.apply(Constants.Blocks.BL_BLUEPRINT.getPath(), () -> LBBlueprintBlock.CODEC);
-                })
-                .register();
+                    r.register(Constants.Blocks.BL_BLUEPRINT.getPath(), () -> LBBlueprintBlock.CODEC);
+                });
         // Items
         Services.PLATFORM.createModObjectRegistry(BuiltInRegistries.ITEM, Constants.MOD_ID)
                 .registerObjects(r -> {
-                    r.apply(Constants.Blocks.BL_BLUEPRINT.getPath(), () -> ModItems.BL_BLUEPRINT);
+                    r.register(Constants.Blocks.BL_BLUEPRINT.getPath(), () -> ModItems.BL_BLUEPRINT);
 
-                    LASER_BLADE = r.apply(Constants.Items.LASER_BLADE.getPath(), () -> ModItems.LASER_BLADE);
-                    r.apply(Constants.Items.LASER_BLADE_FP.getPath(), () -> ModItems.LASER_BLADE_FP);
+                    LASER_BLADE = r.register(Constants.Items.LASER_BLADE.getPath(), () -> ModItems.LASER_BLADE);
+                    r.register(Constants.Items.LASER_BLADE_FP.getPath(), () -> ModItems.LASER_BLADE_FP);
 
-                    r.apply(Constants.Items.LB_BATTERY.getPath(), () -> ModItems.LB_BATTERY);
-                    r.apply(Constants.Items.LB_MEDIUM.getPath(), () -> ModItems.LB_MEDIUM);
-                    r.apply(Constants.Items.LB_EMITTER.getPath(), () -> ModItems.LB_EMITTER);
-                    r.apply(Constants.Items.LB_CASING.getPath(), () -> ModItems.LB_CASING);
-                    r.apply(Constants.Items.LB_CASING_FP.getPath(), () -> ModItems.LB_CASING_FP);
-                })
-                .register();
+                    r.register(Constants.Items.LB_BATTERY.getPath(), () -> ModItems.LB_BATTERY);
+                    r.register(Constants.Items.LB_MEDIUM.getPath(), () -> ModItems.LB_MEDIUM);
+                    r.register(Constants.Items.LB_EMITTER.getPath(), () -> ModItems.LB_EMITTER);
+                    r.register(Constants.Items.LB_CASING.getPath(), () -> ModItems.LB_CASING);
+                    r.register(Constants.Items.LB_CASING_FP.getPath(), () -> ModItems.LB_CASING_FP);
+                });
         // Data component types
         Services.PLATFORM.createModObjectRegistry(BuiltInRegistries.DATA_COMPONENT_TYPE, Constants.MOD_ID)
                 .registerObjects(r -> {
-                    r.apply(Constants.DataComponents.MODEL.getPath(), () -> ModDataComponents.MODEL);
-                    r.apply(Constants.DataComponents.BLEND_MODES.getPath(), () -> ModDataComponents.BLEND_MODES);
-                })
-                .register();
+                    r.register(Constants.DataComponents.MODEL.getPath(), () -> ModDataComponents.MODEL);
+                    r.register(Constants.DataComponents.BLEND_MODES.getPath(), () -> ModDataComponents.BLEND_MODES);
+                });
         // Menus
         Services.PLATFORM.createModObjectRegistry(BuiltInRegistries.MENU, Constants.MOD_ID)
                 .registerObjects(r -> {
-                })
-                .register();
+                });
         // Creative mode tabs
         Services.PLATFORM.createModObjectRegistry(BuiltInRegistries.CREATIVE_MODE_TAB, Constants.MOD_ID)
                 .registerObjects(r -> {
-                    r.apply(Constants.CreativeModeTabs.MAIN.getPath(), ModRegistries::createMainCreativeModeTab);
-                })
-                .register();
+                    r.register(Constants.CreativeModeTabs.MAIN.getPath(), ModRegistries::createMainCreativeModeTab);
+                });
     }
 
     private static CreativeModeTab createMainCreativeModeTab() {
