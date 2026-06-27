765d4d2c02515e8bde67c6d40dd3140986932d7b
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-06-27 23:58:26 +0900

    Update language provider

--- datagen/src/main/java/com/github/iunius118/tolaserblade/data/lang/EnUsLanguageProvider.java
+++ datagen/src/main/java/com/github/iunius118/tolaserblade/data/lang/EnUsLanguageProvider.java
@@ -4,20 +4,20 @@ import net.minecraft.data.PackOutput;
 
 public class EnUsLanguageProvider extends LanguageProviderBase {
 
-	public EnUsLanguageProvider(PackOutput output) {
-		super(output, "en_us");
+    public EnUsLanguageProvider(PackOutput output) {
+        super(output, "en_us");
 
         // Block names
         laserBladeBlueprint = "Laser Blade Blueprint";
 
-		// Item names
-		laserBlade = "Laser Blade";
-		laserBladeFp = "Laser Blade";
-		lbEnergyCell = "LB Energy Cell";
-		laserMedium = "Laser Medium";
-		laserBladeEmitter = "Laser Blade Emitter";
-		laserBladeCasing = "Laser Blade Casing";
-		laserBladeCasingFp = "Laser Blade Casing";
+        // Item names
+        laserBlade = "Laser Blade";
+        laserBladeFp = "Laser Blade";
+        lbEnergyCell = "LB Energy Cell";
+        laserMedium = "Laser Medium";
+        laserBladeEmitter = "Laser Blade Emitter";
+        laserBladeCasing = "Laser Blade Casing";
+        laserBladeCasingFp = "Laser Blade Casing";
 
         // Item tooltips
         tooltipFireResistant = "HEAT RESISTANT 8000";
@@ -25,19 +25,27 @@ public class EnUsLanguageProvider extends LanguageProviderBase {
         // Tags
         tagLaserBlades = "Laser Blades";
         tagLaserBladeEnchantable = "Laser Blade Enchantable";
+        tagBlueprintCanChangeColor = "Blueprint Can Change Color";
+        tagLaserBladeUpgrade = "Laser Blade Upgrade";
+        tagLightElementUpgrade = "Light Element Upgrade";
+        tagRepulsiveForceUpgrade = "Repulsive Force Upgrade";
+        tagLootingUpgrade = "Looting Upgrade";
         tagSensitiveToLaserBlade = "Sensitive to Laser Blade";
 
-		// Enchantments
-		laserBladeEnchantment = "Laser Blade";
-		laserBladeEnchantmentDesc = "Increases attack damage and attack speed of the weapon.";
-		lightElementEnchantment = "Light Element";
-		lightElementEnchantmentDesc = "Increases damage against undead mobs and illagers.";
-		repulsiveForceEnchantment = "Repulsive Force";
-		repulsiveForceEnchantmentDesc =
-				"Allows Laser Blades to block attacks and increases knockback strength at higher levels.";
+        // Enchantments
+        laserBladeEnchantment = "Laser Blade";
+        laserBladeEnchantmentDesc = "Increases attack damage and attack speed of the weapon.";
+        lightElementEnchantment = "Light Element";
+        lightElementEnchantmentDesc = "Increases damage against undead mobs and illagers.";
+        repulsiveForceEnchantment = "Repulsive Force";
+        repulsiveForceEnchantmentDesc =
+                "Allows Laser Blades to block attacks and increases knockback strength at higher levels.";
+
+        // Menus
+        menuBlueprintTitle = "Laser Blade Blueprint";
 
         // Data Packs
         packRepulsiveForceName = "TLB Repulsive Force Pack";
         packRepulsiveForceDescription = "[ToLaserBlade] Add Repulsive Force enchantment";
-	}
+    }
 }
--- datagen/src/main/java/com/github/iunius118/tolaserblade/data/lang/LanguageProviderBase.java
+++ datagen/src/main/java/com/github/iunius118/tolaserblade/data/lang/LanguageProviderBase.java
@@ -7,61 +7,71 @@ import com.github.iunius118.tolaserblade.item.enchantment.ModEnchantments;
 import com.github.iunius118.tolaserblade.tag.ModTags;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.Util;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.LanguageProvider;
 
 public abstract class LanguageProviderBase extends LanguageProvider {
-	// Creative mode tab title
-	public String mainItemGroup = Constants.MOD_NAME;
+    // Creative mode tab title
+    public String mainItemGroup = Constants.MOD_NAME;
     // Block names
     public String laserBladeBlueprint;
-	// Item names
-	public String laserBlade;
-	public String laserBladeFp;
-	public String lbEnergyCell;
-	public String laserMedium;
-	public String laserBladeEmitter;
-	public String laserBladeCasing;
-	public String laserBladeCasingFp;
+    // Item names
+    public String laserBlade;
+    public String laserBladeFp;
+    public String lbEnergyCell;
+    public String laserMedium;
+    public String laserBladeEmitter;
+    public String laserBladeCasing;
+    public String laserBladeCasingFp;
     // Item tooltips
     public String tooltipFireResistant;
     // Tags
     public String tagLaserBlades;
     public String tagLaserBladeEnchantable;
+    public String tagBlueprintCanChangeColor;
+    public String tagLaserBladeUpgrade;
+    public String tagLightElementUpgrade;
+    public String tagRepulsiveForceUpgrade;
+    public String tagLootingUpgrade;
     public String tagSensitiveToLaserBlade;
-	// Enchantments
-	public String laserBladeEnchantment;
-	public String laserBladeEnchantmentDesc;
-	public String lightElementEnchantment;
-	public String lightElementEnchantmentDesc;
-	public String repulsiveForceEnchantment;
-	public String repulsiveForceEnchantmentDesc;
+    // Enchantments
+    public String laserBladeEnchantment;
+    public String laserBladeEnchantmentDesc;
+    public String lightElementEnchantment;
+    public String lightElementEnchantmentDesc;
+    public String repulsiveForceEnchantment;
+    public String repulsiveForceEnchantmentDesc;
+    // Menus
+    public String menuBlueprintTitle;
     // Data Packs
     public String packRepulsiveForceName;
     public String packRepulsiveForceDescription;
 
 
-	public LanguageProviderBase(PackOutput output, String locale) {
-		super(output, Constants.MOD_ID, locale);
-	}
+    public LanguageProviderBase(PackOutput output, String locale) {
+        super(output, Constants.MOD_ID, locale);
+    }
 
-	@Override
-	protected void addTranslations() {
-		// Creative mode tab title
-		add("itemGroup.tolaserblade.main", mainItemGroup);
+    @Override
+    protected void addTranslations() {
+        // Creative mode tab title
+        add("itemGroup.tolaserblade.main", mainItemGroup);
 
         // Block names
         add(ModBlocks.BL_BLUEPRINT, laserBladeBlueprint);
 
-		// Item names
-		add(ModItems.LASER_BLADE, laserBlade);
-		add(ModItems.LASER_BLADE_FP, laserBladeFp);
-		add(ModItems.LB_BATTERY, lbEnergyCell);
-		add(ModItems.LB_MEDIUM, laserMedium);
-		add(ModItems.LB_EMITTER, laserBladeEmitter);
-		add(ModItems.LB_CASING, laserBladeCasing);
-		add(ModItems.LB_CASING_FP, laserBladeCasingFp);
+        // Item names
+        add(ModItems.LASER_BLADE, laserBlade);
+        add(ModItems.LASER_BLADE_FP, laserBladeFp);
+        add(ModItems.LB_BATTERY, lbEnergyCell);
+        add(ModItems.LB_MEDIUM, laserMedium);
+        add(ModItems.LB_EMITTER, laserBladeEmitter);
+        add(ModItems.LB_CASING, laserBladeCasing);
+        add(ModItems.LB_CASING_FP, laserBladeCasingFp);
 
         // Item tooltips
         add("tooltip.tolaserblade.fire_resistant", tooltipFireResistant);
@@ -69,24 +79,52 @@ public abstract class LanguageProviderBase extends LanguageProvider {
         // Tags
         add(ModTags.Items.LASER_BLADES, tagLaserBlades);
         add(ModTags.Items.LASER_BLADE_ENCHANTABLE, tagLaserBladeEnchantable);
+        add(ModTags.Items.BLUEPRINT_CAN_CHANGE_COLOR, tagBlueprintCanChangeColor);
+        add(ModTags.Items.LASER_BLADE_UPGRADE, tagLaserBladeUpgrade);
+        add(ModTags.Items.LIGHT_ELEMENT_UPGRADE, tagLightElementUpgrade);
+        add(ModTags.Items.REPULSIVE_FORCE_UPGRADE, tagRepulsiveForceUpgrade);
+        add(ModTags.Items.LOOTING_UPGRADE, tagLootingUpgrade);
         add(ModTags.EntityTypes.SENSITIVE_TO_LASER_BLADE, tagSensitiveToLaserBlade);
 
-		// Enchantments
-		addEnchantment(ModEnchantments.LASER_BLADE, laserBladeEnchantment, laserBladeEnchantmentDesc);
-		addEnchantment(ModEnchantments.LIGHT_ELEMENT, lightElementEnchantment, lightElementEnchantmentDesc);
-		addEnchantment(ModEnchantments.REPULSIVE_FORCE, repulsiveForceEnchantment, repulsiveForceEnchantmentDesc);
+        // Enchantments
+        addEnchantment(ModEnchantments.LASER_BLADE, laserBladeEnchantment, laserBladeEnchantmentDesc);
+        addEnchantment(ModEnchantments.LIGHT_ELEMENT, lightElementEnchantment, lightElementEnchantmentDesc);
+        addEnchantment(ModEnchantments.REPULSIVE_FORCE, repulsiveForceEnchantment, repulsiveForceEnchantmentDesc);
+
+        // Menus
+        add(Constants.Menus.BLUEPRINT_TITLE, menuBlueprintTitle);
 
         // Data Packs
         add(Constants.DataPacks.REPULSIVE_FORCE.nameKey(), packRepulsiveForceName);
         add(Constants.DataPacks.REPULSIVE_FORCE.descriptionKey(), packRepulsiveForceDescription);
-	}
+    }
 
-	public void addEnchantment(ResourceKey<Enchantment> enchantment, String name, String description) {
-		// Register enchantment name
-		String id = Util.makeDescriptionId("enchantment", enchantment.identifier());
-		add(id, name);
+    @Override
+    public void add(Block key, String name) {
+        if (name != null) super.add(key, name);
+    }
 
-		// Support for Enchantment Descriptions mod
-		add(id + ".desc", description);
-	}
+    @Override
+    public void add(Item key, String name) {
+        if (name != null) super.add(key, name);
+    }
+
+    @Override
+    public void add(String key, String value) {
+        if (value != null) super.add(key, value);
+    }
+
+    @Override
+    public void add(TagKey<?> tagKey, String name) {
+        if (name != null) super.add(tagKey, name);
+    }
+
+    public void addEnchantment(ResourceKey<Enchantment> enchantment, String name, String description) {
+        // Register enchantment name
+        String id = Util.makeDescriptionId("enchantment", enchantment.identifier());
+        add(id, name);
+
+        // Support for Enchantment Descriptions mod
+        add(id + ".desc", description);
+    }
 }
